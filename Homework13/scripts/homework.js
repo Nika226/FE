18d344faea03926fd68f00793899e1e868f5f33e
Veronika <112717708+Nika226@users.noreply.github.com>
2023-02-05 22:02:01 +0100

    Update homework.js

--- Homework13/scripts/homework.js
+++ Homework13/scripts/homework.js
@@ -1,1 +1,25 @@
+// Задан массив users. Для каждого пользователя создать карточку c аватаром, именем, фамилией и мэйлом. Карточка должна включать отображение фото. Почта кликабельна, при нажатии должно открывать приложение почты по умолчанию. Формат почты - Email: example@gmail.com
 
+const users = [
+  {
+  id: 1,
+  email: "michael.lawson@reqres.in",
+  first_name: "Michael",
+  last_name: "Lawson",
+  avatar: "https://reqres.in/img/faces/7-image.jpg"
+  },
+  {
+  id: 2,
+  email: "lindsay.ferguson@reqres.in",
+  first_name: "Lindsay",
+  last_name: "Ferguson",
+  avatar: "https://reqres.in/img/faces/8-image.jpg"
+  },
+  {
+  id: 3,
+  email: "tobias.funke@reqres.in",
+  first_name: "Tobias",
+  last_name: "Funke",
+  avatar: "https://reqres.in/img/faces/9-image.jpg"
+  }
+];
