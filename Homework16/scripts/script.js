454b82d1a8355a63e3a7028c7fd60638024abb00
Veronika <112717708+Nika226@users.noreply.github.com>
2023-02-19 21:50:50 +0100

    Update script.js

--- Homework16/scripts/script.js
+++ Homework16/scripts/script.js
@@ -1,1 +1,19 @@
+// const click_btn = document.querySelector('.click_btn');
 
+// 1. При клике на кнопку выводить в консоль сообщение "Клик!"
+
+// click_btn.addEventListener('click', () => console.log('Клик!'));
+
+
+// 2. При клике на кнопку выводить в консоль сообщение со счетчиком: "Кнопка нажата Х раз(а)"
+
+// let counter_num = 0;
+// click_btn.addEventListener('click', () => console.log(`Кнопка нажата ${counter_num++} раз(а)`));
+
+// click_btn.addEventListener('click', () => {
+//   counter_num++
+//   counter_num === 2 || counter_num === 3 || counter_num === 4 || 
+//   (counter_num > 20 && (counter_num % 10 === 2 || counter_num % 10 === 3 || counter_num % 10 === 4))
+//   ? console.log(`Кнопка нажата ${counter_num} раза`)
+//   : console.log(`Кнопка нажата ${counter_num} раз`)
+// });
