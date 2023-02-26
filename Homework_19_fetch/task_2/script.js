5741d077ce1bd6e4331b46846f12d812a5aefcb5
Veronika <112717708+Nika226@users.noreply.github.com>
2023-02-26 17:44:20 +0100

    Update script.js

--- Homework_19_fetch/task_2/script.js
+++ Homework_19_fetch/task_2/script.js
@@ -1,1 +1,27 @@
+const cards_container = document.querySelector('.cards_container');
 
+const render = json => {
+  json.forEach(({ first_name, avatar }) => {
+    const cardElem = document.createElement('div');
+    const nameElem = document.createElement('p');
+    const avatarElem = document.createElement('img');
+
+    nameElem.innerText = first_name;
+
+    avatarElem.src = avatar;
+    avatarElem.alt = first_name;
+
+    cardElem.classList.add('card');
+
+    cardElem.append(nameElem, avatarElem);
+    cards_container.append(cardElem);
+  })
+}
+
+const getUsers = id => {
+  fetch(`https://reqres.in/api/users?page=${id}`)
+    .then(res => res.json())
+    .then(json => render(json.data))
+}
+
+getUsers(1);
