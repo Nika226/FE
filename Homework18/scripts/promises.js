084f3dd6dc1a5c93c413c46d0dc4051eed1c285c
Veronika <112717708+Nika226@users.noreply.github.com>
2023-02-26 17:28:12 +0100

    Update promises.js

--- Homework18/scripts/promises.js
+++ Homework18/scripts/promises.js
@@ -1,1 +1,14 @@
+let number = 12;
+
+// setTimeout(() => {
+//   number = 100;
+//   console.log(number);
+// }, 3000);
+
+
+const new_number = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(number = 100)
+  }, 3000)
+}).then(() => console.log(number));
 
