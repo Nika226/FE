262aafe09932f8a18e25d8d44404f2ad8c9f536b
Veronika <112717708+Nika226@users.noreply.github.com>
2023-03-05 21:27:07 +0100

    Update script.js

--- Homework_21_nav_menu/script.js
+++ Homework_21_nav_menu/script.js
@@ -1,1 +1,6 @@
+const menu_icon = document.querySelector ('#menu_icon');
+const nav_menu = document.querySelector('.nav_menu');
 
+menu_icon.addEventListener('click', () => {
+nav_menu.classList.toggle('activ')
+});
